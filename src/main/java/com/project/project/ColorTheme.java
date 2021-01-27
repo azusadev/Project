0cd7a117ecaa7e90114026ea0d5e2dd9f5904ccb
Reden Valerio <azusa.dev@gmail.com>
2021-01-27 14:26:48 +0800

    initial commit

---
+++ src/main/java/com/project/project/ColorTheme.java
@@ -0,0 +1,21 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.project.project;
+
+import java.awt.Color;
+import java.util.ArrayList;
+
+/**
+ *
+ * @author reden
+ */
+public class ColorTheme {
+    public static Color primaryColor = Color.decode("#162431");
+    public static Color secondaryColor = Color.decode("#c5d6de");
+    public static Color accentColor = Color.decode("#263352");
+    public static Color highlightColor = Color.decode("#51596b");
+    public static Color secondaryHighlightColor = Color.decode("#3c424f");
+}
